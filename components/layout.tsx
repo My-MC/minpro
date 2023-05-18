4680b5f9408a18c97e1ab121f3c35c2353b694bc
My <84212641+My-MC@users.noreply.github.com>
2023-05-18 08:39:09 +0000

    Add eslint-plugin-github and follow its rules

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,8 @@
+// eslint-disable-next-line camelcase
 import { Noto_Sans_JP } from "next/font/google";
 import Header from "./Header";
 import Footer from "./Footer";
+import { ReactNode } from "react";
 
 const noto = Noto_Sans_JP({
   weight: ["400", "500"],
@@ -8,7 +10,7 @@ const noto = Noto_Sans_JP({
   subsets: ["latin"],
 });
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <div className={noto.className}>
